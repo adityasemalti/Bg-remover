0ac3f6a7fe1ca1c37fd5f49330a1b6368f141157
adityasemalti <adityasemalti535@gmail.com>
2025-07-06 17:52:16 +0530

    first commit

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express'
 import cors from 'cors'
 import { connectDb } from './configs/mongoDb.js';
+import userRouter from './routes/userRoutes.js';
 
 const app = express()
 const PORT = process.env.PORT || 4000
@@ -12,5 +13,7 @@ app.use(cors())
 app.get('/',(req,res)=>{
     res.send("Api working")
 })
+app.use('/api/user',userRouter)
+
 connectDb()
 app.listen(PORT,()=>{console.log(`Server running on ${PORT}`)})
